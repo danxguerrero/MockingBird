e4b4a2bd1f357add6c159e080e7e4e4d70844717
danxguerrero <danxguerrero@yahoo.com>
2025-05-27 23:13:34 -0400

    Add styling to Header component

--- mockingbird/src/app/components/Header.tsx
+++ mockingbird/src/app/components/Header.tsx
@@ -1,8 +1,18 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
 export const Header = () => {
+    const pathname = usePathname()
+
     return (
-        <div className="border border-red-600 h-12 p-6 py-3 flex justify-between">
+        <div className="border-b border-gray-700 h-12 p-6 py-3 flex justify-between">
             <p>MockingBird</p>
-            <p>User</p>
+            {pathname === '/' ? 
+                <Link href="/interview" className="inline-block bg-lime-300 text-black px-3 rounded-lg hover:bg-lime-500 hover:text-white transition-colors">Start New Interview</Link> 
+                : 
+                <Link href="/" className="inline-block bg-lime-300 text-black px-6 rounded-lg hover:bg-lime-500 hover:text-white transition-colors">Home</Link>
+            }
         </div>
     )
 }
