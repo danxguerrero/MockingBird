ff98a9c9450324765dc56e57775a1f517b0027fe
danxguerrero <danxguerrero@yahoo.com>
2025-05-30 19:54:00 -0400

    Tweaked Styling for Header component

--- mockingbird/src/app/components/Header.tsx
+++ mockingbird/src/app/components/Header.tsx
@@ -6,12 +6,12 @@ export const Header = () => {
     const pathname = usePathname()
 
     return (
-        <div className="border-b border-gray-700 h-12 p-6 py-3 flex justify-between">
+        <div className="border-b border-gray-700 h-16 p-6 flex justify-between items-center">
             <p>MockingBird</p>
             {pathname === '/' ? 
                 <Link href="/interview" className="inline-block bg-lime-300 text-black px-3 rounded-lg hover:bg-lime-500 hover:text-white transition-colors">Start New Interview</Link> 
                 : 
-                <Link href="/" className="inline-block bg-lime-300 text-black px-6 rounded-lg hover:bg-lime-500 hover:text-white transition-colors">Home</Link>
+                <Link href="/" className="inline-block bg-lime-300 text-black px-3 rounded-lg hover:bg-lime-500 hover:text-white transition-colors">Home</Link>
             }
         </div>
     )
