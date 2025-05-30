a38df3092822f36d1c750d47a3f194b05e6d7742
danxguerrero <danxguerrero@yahoo.com>
2025-05-30 19:41:25 -0400

    Created and styled Footer component. Closes #4

---
+++ mockingbird/src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <div className="border-t border-gray-700 h-12 py-3 flex justify-center">
+        <p>Created by Daniel Guerrero</p>
+      </div>
+    )
+}
