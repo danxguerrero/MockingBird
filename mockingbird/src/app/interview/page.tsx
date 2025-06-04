8a7035899001ec8bc32b4ed25dd1ae8e1b0a04f1
danxguerrero <danxguerrero@yahoo.com>
2025-06-04 17:46:41 -0400

    Changed layout of Interview page.

--- mockingbird/src/app/interview/page.tsx
+++ mockingbird/src/app/interview/page.tsx
@@ -1,10 +1,23 @@
 export default function Page() {
     return (
-        <>
-            <div className="flex flex-1">
-                <div className="border border-blue-600 w-150"></div>
-                <div className="border border-green-600 w-310 flex-1"></div>
+        <div className="flex flex-1">
+            <div className="bg-color-red-500 w-2/5 flex flex-col">
+                <div className="h-1/3">
+                    <p>Timer</p>
+                </div>
+                <div className="h-2/3">
+                    <p>Question</p>
+                </div>
             </div>
-        </>
+            <div className="w-full flex flex-col">
+                <div className="h-1/2">
+                    <p>Text Editor</p>
+                </div>
+                <div className="h-1/2">
+                    <p>Chat</p>
+                </div>
+
+            </div>
+        </div>
     )
 }
