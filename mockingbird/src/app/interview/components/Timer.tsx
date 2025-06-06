58c3a6e6f76916137c9f00028ad02e1d9bb87b41
danxguerrero <danxguerrero@yahoo.com>
2025-06-06 16:14:36 -0400

    Added functionality for timer to count down 45 minutes then deactivate interview.

--- mockingbird/src/app/interview/components/Timer.tsx
+++ mockingbird/src/app/interview/components/Timer.tsx
@@ -1,13 +1,36 @@
 'use client'
 
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 
 export const Timer = () => {
-    const [interviewActive, setInterviewActive] = useState(false)
+    const [interviewActive, setInterviewActive] = useState<boolean>(false)
+    const [time, setTime] = useState<number>(45 * 60) // 45 minutes in seconds
+    const [seconds, setSeconds] = useState<number>(0)
+    const [minutes, setMinutes] = useState<number>(45)
+
+    useEffect(() => {
+        if (interviewActive) {
+            setTimeout(() => {
+                if (time === 0) {
+                    setInterviewActive(false)
+                    return;
+                }
+
+                setTime(time - 1)
+                setMinutes(Math.floor((time - 1) / 60))
+                setSeconds((time - 1) % 60)
+            }, 1000)
+        }
+    }, [time, interviewActive])
 
     return (
-        <div>
-            {interviewActive ? <p>Interview Active</p> : <button onClick={()=>setInterviewActive(true)}>Start Interview</button>}
+        <div className="border border-red-500 w-full">
+            {interviewActive ? (
+                <h2>{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}`: seconds}</h2>
+                ) : (
+                <button onClick={() => setInterviewActive(true)}>Start Interview</button>
+                )
+            }
         </div>
     )
 }
--- mockingbird/src/app/interview/page.tsx
+++ mockingbird/src/app/interview/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div className="flex flex-1">
             <div className="bg-color-red-500 w-2/5 flex flex-col">
-                <div className="h-1/3">
+                <div className="h-1/3 flex">
                     <Timer />
                 </div>
                 <div className="h-2/3">
