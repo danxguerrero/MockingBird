cdd0de7d57a9781699ddc604d05c53051d30b5c8
danxguerrero <danxguerrero@yahoo.com>
2025-06-07 23:50:03 -0400

    Adjusted the structure of the Timer component to label the minutes and seconds

--- mockingbird/src/app/interview/components/Timer.tsx
+++ mockingbird/src/app/interview/components/Timer.tsx
@@ -24,13 +24,21 @@ export const Timer = () => {
     }, [time, interviewActive])
 
     return (
-        <div className="border border-red-500 w-full">
+        <div className="border border-red-500 w-full flex flex-col items-center justify-center">
             {interviewActive ? (
-                <h2>{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}`: seconds}</h2>
-                ) : (
+                <div className="flex space-x-4">
+                    <div className="flex flex-col space-x-4  gap-4 items-center justify-center">
+                        <p className="text-4xl">{minutes < 10 ? `0${minutes}` : minutes}</p>
+                        <p>Minutes</p>
+                    </div>
+                    <div className="flex flex-col space-x-4 gap-4 items-center justify-center">
+                        <p className="text-4xl">{seconds < 10 ? `0${seconds}` : seconds}</p>
+                        <p>Seconds</p>
+                    </div>
+                </div>
+            ) : (
                 <button onClick={() => setInterviewActive(true)}>Start Interview</button>
-                )
-            }
+            )}
         </div>
     )
 }
