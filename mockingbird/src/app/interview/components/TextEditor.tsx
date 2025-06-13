651c717b1bfd1121ec638f20552b7a8a3bb92351
danxguerrero <danxguerrero@yahoo.com>
2025-06-13 12:58:49 -0400

    Implemented Monaco Editor into TextEditor component. closes #9

--- mockingbird/src/app/interview/components/TextEditor.tsx
+++ mockingbird/src/app/interview/components/TextEditor.tsx
@@ -1,3 +1,35 @@
+'use client'
+
+import Editor from "@monaco-editor/react"
+
+const editorOptions = {
+    minimap: { enabled: false },
+    scrollBeyondLastLine: false,
+    fontSize: 14,
+    roundedSelection: false,
+    scrollbar: {
+        vertical: "visible" as const,
+        horizontal: "visible" as const
+    },
+    // Disable all auto-completion features
+    suggestOnTriggerCharacters: false,
+    quickSuggestions: false,
+    parameterHints: { enabled: false },
+    suggest: { showWords: false },
+    autoClosingBrackets: "true",
+    autoClosingQuotes: "true",
+    autoIndent: "none",
+    formatOnPaste: false,
+    formatOnType: false
+}
+
 export const TextEditor = () => {
-    return <p>Text Editor</p>
+    return (
+    <Editor 
+        height="100%" 
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        options = {editorOptions}
+    />
+    )
 }
