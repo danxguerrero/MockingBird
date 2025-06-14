ada4af088672a089a3a4049600d8e64a37faa7b3
danxguerrero <danxguerrero@yahoo.com>
2025-06-14 15:12:18 -0400

    Added typing to editor options to prevent compilation errors.

--- mockingbird/src/app/interview/components/TextEditor.tsx
+++ mockingbird/src/app/interview/components/TextEditor.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import Editor from "@monaco-editor/react"
+import { useRef } from 'react'
+import Editor, { OnMount } from "@monaco-editor/react"
+import { editor } from 'monaco-editor'
 
-const editorOptions = {
+const editorOptions: editor.IStandaloneEditorConstructionOptions = {
     minimap: { enabled: false },
     scrollBeyondLastLine: false,
     fontSize: 14,
@@ -16,20 +18,37 @@ const editorOptions = {
     quickSuggestions: false,
     parameterHints: { enabled: false },
     suggest: { showWords: false },
-    autoClosingBrackets: "true",
-    autoClosingQuotes: "true",
+    autoClosingBrackets: "always",
+    autoClosingQuotes: "always",
     autoIndent: "none",
     formatOnPaste: false,
     formatOnType: false
 }
 
 export const TextEditor = () => {
-    return (
-    <Editor 
-        height="100%" 
-        defaultLanguage="javascript"
-        theme="vs-dark"
-        options = {editorOptions}
-    />
+
+    const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null)
+    const handleEditorDidMount: OnMount = (editor) => {
+        editorRef.current = editor
+    }
+    function showValue() {
+        if (editorRef.current) {
+        alert(editorRef.current.getValue());
+      }
+    }
+
+   return (
+        <div className="h-full w-full">
+        <Editor 
+            height="90%" 
+            defaultLanguage="javascript"
+            theme="vs-dark"
+            defaultValue='//Enter your code here'
+            options = {editorOptions}
+            onMount={handleEditorDidMount}
+        />
+        <button onClick={showValue}>Show Content</button>
+        </div>
+
     )
 }
