096a4f96b4485cb234205ea2f9afae7a436e19fe
danxguerrero <danxguerrero@yahoo.com>
2025-06-18 17:17:26 -0400

    Create a basic skeleton for Chat component

--- mockingbird/src/app/interview/components/Chat.tsx
+++ mockingbird/src/app/interview/components/Chat.tsx
@@ -1,5 +1,17 @@
 'use client'
 
+import { useState } from 'react'
+
 export const Chat = () => {
-    return <p>Chat goes here</p>
+    const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState("")
+    return <div className="h-full">
+        <div className="h-9/10 border border-blue-500">
+            {messages.map(message => <p>{message}</p>)}
+        </div>
+        <form className="border border-red-500 h-1/10 flex">
+            <input type="text" className="w-9/10 border rounded-lg"></input>
+            <input type="submit" value="Send Message" className="bg-lime-500 w-1/10 rounded-lg"></input>
+        </form>
+    </div>
 }
