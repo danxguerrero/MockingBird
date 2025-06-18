25b17b6a8523c5df69d76bf453e52edcdf2592ee
danxguerrero <danxguerrero@yahoo.com>
2025-06-18 17:49:54 -0400

    Implemented basic chat function so that users input is displayed on screen.

--- mockingbird/src/app/interview/components/Chat.tsx
+++ mockingbird/src/app/interview/components/Chat.tsx
@@ -3,14 +3,29 @@
 import { useState } from 'react'
 
 export const Chat = () => {
-    const [messages, setMessages] = useState([])
-    const [message, setMessage] = useState("")
+    const [messages, setMessages] = useState<string[]>([])
+    const [message, setMessage] = useState<string>("")
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setMessages([...messages, message])
+        setMessage("")
+    }
+
     return <div className="h-full">
-        <div className="h-9/10 border border-blue-500">
-            {messages.map(message => <p>{message}</p>)}
+        <div className="h-9/10">
+            {messages.map((userMessage, idx)=> <p key={idx}>{userMessage}</p>)}
         </div>
-        <form className="border border-red-500 h-1/10 flex">
-            <input type="text" className="w-9/10 border rounded-lg"></input>
+        <form 
+            className="h-1/10 flex"
+            onSubmit={handleSubmit}
+        >
+            <input 
+                type="text" 
+                value={message}
+                className="w-9/10 border rounded-lg"
+                onChange={e => setMessage(e.target.value)}
+            />
             <input type="submit" value="Send Message" className="bg-lime-500 w-1/10 rounded-lg"></input>
         </form>
     </div>
