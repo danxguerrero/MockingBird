94545431001f34ca3688745bdc0900393b8e25c4
danxguerrero <danxguerrero@yahoo.com>
2025-07-08 18:42:00 -0400

    Get messages from local storage on Feedback page

--- mockingbird/src/app/feedback/page.tsx
+++ mockingbird/src/app/feedback/page.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import type { Message } from '../interview/types'
+// import { generateFeedback } from './actions'
+
 export default function FeedbackPage() {
-    return <div className="flex-1">Feedback Page</div>
+    const [messages, setMessages] = useState<Message[]>([])
+    const [feedback, setFeedback] = useState<string>("")
+
+    useEffect(() => {
+        const saved = localStorage.getItem('messages')
+        if (saved) {
+            try {
+                setMessages(JSON.parse(saved))
+            } catch (error) {
+                console.error("Unable to set generate Feedback: ", error)
+                setMessages([])
+            }
+        }
+    }, [])
+
+    return (
+        <div className="flex-1">
+            <h1>Interview Feedback</h1>
+            {messages.length > 0 ? messages.map((msg, idx) => (
+                <div key={idx}>
+                    <strong>{msg.sender}:</strong> {msg.text}
+                </div>
+            )): "No Chat messages found"}
+        </div>
+    )
 }
