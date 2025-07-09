c088f5e72a9e14b80b788554062825f0b58b62ff
danxguerrero <danxguerrero@yahoo.com>
2025-07-09 19:35:19 -0400

    Created useFeedback custom hook to generate feedback

---
+++ mockingbird/src/app/feedback/hooks/useFeedback.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import type { Message } from '../../interview/types'
+import { generateFeedback } from '../actions'
+
+export const useFeedback = (messages: Message[]) => {
+    const [feedback, setFeedback] = useState<string>("")
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        if (messages.length > 1) {
+            setIsLoading(true)
+            setError(null)
+
+            generateFeedback(messages)
+                .then((result) => {
+                    if (result) {
+                        setFeedback(result)
+                    }
+                })
+                .catch ((error) => {
+                    console.error("Unable to generate feedback: ", error)
+                    setError("Failed to generate feedback.")
+                })
+                .finally(() => {
+                    setIsLoading(false)
+                })
+        }
+    }, [messages])
+    
+    return { feedback, isLoading, error}
+}
--- mockingbird/src/app/feedback/page.tsx
+++ mockingbird/src/app/feedback/page.tsx
@@ -2,11 +2,10 @@
 
 import { useEffect, useState } from 'react'
 import type { Message } from '../interview/types'
-// import { generateFeedback } from './actions'
+import { generateFeedback } from './actions'
 
 export default function FeedbackPage() {
     const [messages, setMessages] = useState<Message[]>([])
-    const [feedback, setFeedback] = useState<string>("")
 
     useEffect(() => {
         const saved = localStorage.getItem('messages')
@@ -14,7 +13,7 @@ export default function FeedbackPage() {
             try {
                 setMessages(JSON.parse(saved))
             } catch (error) {
-                console.error("Unable to set generate Feedback: ", error)
+                console.error("Unable to set messages: ", error)
                 setMessages([])
             }
         }
@@ -23,7 +22,7 @@ export default function FeedbackPage() {
     return (
         <div className="flex-1">
             <h1>Interview Feedback</h1>
-            {messages.length > 0 ? messages.map((msg, idx) => (
+            {messages.length > 1 ? messages.map((msg, idx) => (
                 <div key={idx}>
                     <strong>{msg.sender}:</strong> {msg.text}
                 </div>
