15ff3dfc285ce48147c4a3240582be2e33b3c72d
danxguerrero <danxguerrero@yahoo.com>
2025-08-13 20:04:28 -0400

    Update InterviewStateManager to reset state based on interview data presence, enhancing state management logic

--- mockingbird/src/app/components/InterviewStateManager.tsx
+++ mockingbird/src/app/components/InterviewStateManager.tsx
@@ -6,14 +6,14 @@ import { useInterview } from '../interview/components/InterviewContext'
 
 export const InterviewStateManager = () => {
     const pathname = usePathname()
-    const { resetInterview, interviewActive } = useInterview()
+    const { resetInterview, interviewActive, question, messages } = useInterview()
 
     useEffect(() => {
-        // If we're not on the interview page and interview is active, reset the state
-        if (pathname !== '/interview' && interviewActive) {
+        // If we're not on the interview page and we have any interview data, reset the state
+        if (pathname !== '/interview' && (interviewActive || question || messages.length > 0)) {
             resetInterview()
         }
-    }, [pathname, interviewActive, resetInterview])
+    }, [pathname, interviewActive, question, messages, resetInterview])
 
     // This component doesn't render anything, it just manages state
     return null
