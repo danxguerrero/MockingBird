8ca3be5678295fd6f8a27b8830707bc5980d21d7
danxguerrero <danxguerrero@yahoo.com>
2025-08-15 16:13:25 -0400

    Refactor InterviewContent component to enhance layout with borders and rounded corners for better visual separation of elements

--- mockingbird/src/app/interview/components/InterviewContent.tsx
+++ mockingbird/src/app/interview/components/InterviewContent.tsx
@@ -13,20 +13,20 @@ export const InterviewContent = () => {
     return (
         <div className="flex flex-1 h-screen">
             <div className="w-2/5 flex flex-col">
-                <div className="h-1/3 flex">
+                <div className="h-1/3 flex border rounded-lg m-4">
                     <Timer />
                 </div>
-                <div className="h-2/3">
+                <div className="h-2/3 border rounded-lg mx-4 mb-4">
                     {interviewActive && <Question />}
                 </div>
             </div>
             <div className={`w-full flex flex-col h-[90vh] min-h-0 ${interviewActive ? '' : 'justify-center items-center'}`}>
                 {interviewActive && (
                     <>
-                        <div className="flex-1 overflow-hidden min-h-0">
+                        <div className="flex-1 overflow-hidden min-h-0 border rounded-lg m-4">
                             <TextEditor />
                         </div>
-                        <div className="flex-1 overflow-hidden min-h-0">
+                        <div className="flex-1 overflow-hidden min-h-0 border rounded-lg mx-4">
                             <Chat />
                         </div>
                     </>
